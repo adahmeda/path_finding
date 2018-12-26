85372417174725f50605f7451fdf8a6e19dcc749
aegan123 <juhani@fmail.co.uk>
2018-12-26 11:43:57 +0200

    Initial commit

---
+++ build/CMakeFiles/path_finding.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/path_finding.dir/path_finding.cpp.o"
+  "path_finding.pdb"
+  "path_finding"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/path_finding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
